603fb9c0a9daa994f2c8683338e1a473f1a1a98d
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-21 20:25:41 +0200

    include udp server example

--- example/server.php
+++ example/server.php
@@ -1,7 +1,9 @@
 <?php
 
 use GuzzleHttp\Psr7\Response;
+use Onion\Framework\EventLoop\Stream\Interfaces\StreamInterface as TcpStream;
 use Onion\Framework\Server\HttpServer as Server;
+use Onion\Framework\Server\Udp\Interfaces\StreamInterface as UdpStream;
 use Psr\Http\Message\ServerRequestInterface;
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -9,8 +11,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$server = new Server('0.0.0.0', 1337, Server::TYPE_TCP);
-$server->addListener('0.0.0.0', 1338, Server::TYPE_TCP | Server::TYPE_SECURE);
+$server = new Server();
+$server->addListener('0.0.0.0', 1337, Server::TYPE_TCP);
+$server->addListener('0.0.0.0', 1338, Server::TYPE_TCP);
+$server->addListener('0.0.0.0', 1339, Server::TYPE_UDP);
 
 $server->on('start', function () {
     echo "Start\n";
@@ -39,6 +43,25 @@ $server->on('request', function (ServerRequestInterface $request) {
     return new Response(200, [
         'content-type' => ['text/html'],
     ], 'Hello, World!');
+});
+
+// $server->on('receive', function (TcpStream $stream, $data) {
+//     // $stream->block();
+//     echo "Receive\n";
+
+//     $data = $stream->read();
+//     echo "Data {$data}\n";
+
+//     $stream->write($data . "Response");
+// });
+
+$server->on('connect', function () {
+    echo 'Connected';
+});
+
+$server->on('packet', function (UdpStream $stream, $data, $address) {
+    echo "Packet\n";
+    $stream->write($stream->read(), $address);
 });
 
 $server->start();
