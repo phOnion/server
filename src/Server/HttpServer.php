492e9718e299ccd4c8cae39230619cc2b29ab023
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-23 21:56:57 +0200

    improvements to http server

--- src/Server/HttpServer.php
+++ src/Server/HttpServer.php
@@ -2,30 +2,23 @@
 namespace Onion\Framework\Server;
 
 use GuzzleHttp\Psr7\Response;
-use Onion\Framework\EventLoop\Stream\Stream;
+use GuzzleHttp\Stream\StreamInterface;
+use Onion\Framework\Promise\Interfaces\PromiseInterface;
 use Onion\Framework\Promise\Promise;
 use Onion\Framework\Promise\RejectedPromise;
+use Onion\Framework\Server\Interfaces\ServerInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class HttpServer extends Server
+class HttpServer extends Server implements ServerInterface
 {
     public function __construct()
     {
-        parent::on('receive', function (Stream $stream) {
+        parent::on('connect', function () {});
+        parent::on('close', function () {});
+        parent::on('receive', function (StreamInterface $stream) {
             try {
-                $buffer = '';
-                while (!$stream->eof()) {
-                    usleep(1);
-                    $data = $stream->read(-1);
-
-                    if (strlen($buffer) !== 0 && strlen($data) === 0) {
-                        break;
-                    }
-
-                    $buffer .= $data;
-                }
-                $request = build_request($buffer);
-                if ($request->getHeaderLine('Content-Length') > $this->getMaxPackageSize()) {
+                $request = build_request($stream->getContents());
+                if ($request->getHeaderLine('Content-Length') > parent::getMaxPackageSize()) {
                     $promise = new Promise(function ($resolve) use ($request) {
                         $resolve(new Response($request->hasHeader('Expect') ? 417 : 413, [
                             'content-type' => 'text/plain',
@@ -42,9 +35,6 @@ class HttpServer extends Server
 
                 $promise->then(function (ResponseInterface $response) use ($stream) {
                     send_response($response, $stream);
-
-                })->finally(function () use ($stream) {
-                    $stream->close();
                 });
             } catch (\RuntimeException $ex) {
                 $stream->close();
@@ -52,23 +42,34 @@ class HttpServer extends Server
         });
     }
 
-    protected function trigger(string $event, ...$args)
+    protected function trigger(string $event, ...$args): PromiseInterface
     {
-        if ($event !== 'connect') {
+        if (strtolower($event) !== 'connect' || strtolower($event) !== 'receive') {
             return parent::trigger($event, ...$args);
         }
 
-        return new RejectedPromise(new \LogicException("Not allowed to trigger unsupported events"));
+        return new RejectedPromise(new \LogicException("Not allowed to trigger unsupported events ({$event})"));
     }
 
-    public function on(string $event, callable $callback)
+    public function on(string $event, callable $callback): void
     {
-        if (strtolower($event) === 'receive') {
+        if (strtolower($event) === 'receive' || strtolower($event) === 'connect') {
             throw new \RuntimeException(
                 "Binding on 'receive' is not allowed for HTTP server"
             );
         }
 
         parent::on($event, $callback);
+    }
+
+    public function addListener(string $address, ?int $port = 0, int $type = 0, array $options = []): void
+    {
+        if (($type & self::TYPE_UDP) === self::TYPE_UDP) {
+            throw new \InvalidArgumentException(
+                "Unable to add UDP listener to HTTP server"
+            );
+        }
+
+        parent::addListener($address, $port, $type, $options);
     }
 }
