08bcc997e8941a429b5f8035d23b0366dab085f3
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-23 21:57:21 +0200

    add return types to server interface

--- src/Server/Interfaces/ServerInterface.php
+++ src/Server/Interfaces/ServerInterface.php
@@ -8,8 +8,8 @@ interface ServerInterface
     public const TYPE_SOCK = 4;
     public const TYPE_SECURE = 8;
 
-    public function start();
-    public function addListener(string $address, int $port, int $type, array $options = []);
-    public function on(string $event, callable $callback);
-    public function set(array $configuration);
+    public function start(): void;
+    public function addListener(string $address, int $port, int $type, array $options = []): void;
+    public function on(string $event, callable $callback): void;
+    public function set(array $configuration): void;
 }
