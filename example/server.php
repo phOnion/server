e756a7da8f4e1f174c7ec0cde50fbbca090cccb2
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-23 14:02:08 +0200

    update server example

--- example/server.php
+++ example/server.php
@@ -2,7 +2,7 @@
 
 use GuzzleHttp\Psr7\Response;
 use Onion\Framework\EventLoop\Stream\Interfaces\StreamInterface as TcpStream;
-use Onion\Framework\Server\HttpServer as Server;
+use Onion\Framework\Server\Server as Server;
 use Onion\Framework\Server\Udp\Interfaces\StreamInterface as UdpStream;
 use Psr\Http\Message\ServerRequestInterface;
 require __DIR__ . '/../vendor/autoload.php';
@@ -19,44 +19,54 @@ $server->addListener('0.0.0.0', 1339, Server::TYPE_UDP);
 $server->on('start', function () {
     echo "Start\n";
 });
-$server->set([
-    'ssl_cert_file' => __DIR__ . '/../localhost.cert',
-    'ssl_key_file' => __DIR__ . '/../localhost.key',
-    'ssl_allow_self_signed' => true,
-    'ssl_verify_peer' => false,
-]);
+// $server->set([
+//     'ssl_cert_file' => __DIR__ . '/../localhost.cert',
+//     'ssl_key_file' => __DIR__ . '/../localhost.key',
+//     'ssl_allow_self_signed' => true,
+//     'ssl_verify_peer' => false,
+// ]);
 
-$server->on('request', function (ServerRequestInterface $request) {
-    $files = $request->getUploadedFiles();
-    if (count($files) > 0) {
-        return new Response(200, [
-            'content-type' => ['application/octet-stream'],
-            'content-disposition' => [
-                'attachment; filename="test.jpeg"',
-            ],
-            'content-length' => [
-                $files[0]->getSize()
-            ]
-        ], $files[0]->getStream());
-    }
+// $server->on('request', function (ServerRequestInterface $request) {
+//     $files = $request->getUploadedFiles();
+//     if (count($files) > 0) {
+//         return new Response(200, [
+//             'content-type' => ['application/octet-stream'],
+//             'content-disposition' => [
+//                 'attachment; filename="test.jpeg"',
+//             ],
+//             'content-length' => [
+//                 $files[0]->getSize()
+//             ]
+//         ], $files[0]->getStream());
+//     }
 
-    return new Response(200, [
-        'content-type' => ['text/html'],
-    ], 'Hello, World!');
-});
-
-// $server->on('receive', function (TcpStream $stream, $data) {
-//     // $stream->block();
-//     echo "Receive\n";
-
-//     $data = $stream->read();
-//     echo "Data {$data}\n";
-
-//     $stream->write($data . "Response");
+//     return new Response(200, [
+//         'content-type' => ['text/html'],
+//     ], 'Hello, World!');
 // });
 
+$server->on('receive', function (TcpStream $stream, $data) {
+    echo "Receive\n";
+
+    $buffer = '';
+    while ($stream->isClosed()) {
+        $data = $stream->read();
+        $buffer .= $data;
+        $stream->write($buffer);
+        if (strlen($buffer) !== 0 && strlen($data) === 0) {
+            break;
+        }
+    }
+
+    $stream->write($buffer . "Response");
+});
+
 $server->on('connect', function () {
-    echo 'Connected';
+    echo "Connected\n";
+});
+
+$server->on('close', function () {
+    echo "Closed\n";
 });
 
 $server->on('packet', function (UdpStream $stream, $data, $address) {
