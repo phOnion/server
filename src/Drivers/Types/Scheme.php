1c59f9d2541dbe45888923a85108d4c8139c801b
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-03-05 22:00:33 +0200

    styling fixes

--- src/Drivers/Types/Scheme.php
+++ src/Drivers/Types/Scheme.php
@@ -4,8 +4,8 @@ namespace Onion\Framework\Server\Drivers\Types;
 
 enum Scheme: string
 {
-case TCP = 'tcp';
-case UNIX = 'unix';
-case UDP = 'udp';
-case UDG = 'udg';
-    }
+    case TCP = 'tcp';
+    case UNIX = 'unix';
+    case UDP = 'udp';
+    case UDG = 'udg';
+}
