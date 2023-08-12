de9b794d04a94cd188ab88d7d5bb958a035a9159
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-08-13 01:51:28 +0300

    update example for server

--- example/server.php
+++ example/server.php
@@ -4,6 +4,7 @@ use Onion\Framework\Event\Dispatcher;
 use Onion\Framework\Event\ListenerProviders\AggregateProvider;
 use Onion\Framework\Event\ListenerProviders\SimpleProvider;
 use Onion\Framework\Server\Drivers\NetworkDriver;
+use Onion\Framework\Server\Drivers\Types\Scheme;
 use Onion\Framework\Server\Events\CloseEvent;
 use Onion\Framework\Server\Events\ConnectEvent;
 use Onion\Framework\Server\Events\MessageEvent;
@@ -27,7 +28,7 @@ $provider->addProvider(new SimpleProvider([
         echo "Started\n";
     }],
     ConnectEvent::class => [
-        new CryptoListener,
+        // new CryptoListener,
         function () {
             echo "Connected\n";
         }
@@ -50,7 +51,7 @@ $provider->addProvider(new SimpleProvider([
 ]));
 $dispatcher = new Dispatcher($provider);
 
-$baseListener = new NetworkDriver($dispatcher);
+// $baseListener = new NetworkDriver($dispatcher);
 // $secureCtx = new SecureContext;
 // $secureCtx->setLocalCert(__DIR__ . '/localhost.cert');
 // $secureCtx->setLocalKey(__DIR__ . '/localhost.key');
@@ -62,11 +63,10 @@ $baseListener = new NetworkDriver($dispatcher);
 
 $server = new Server($dispatcher);
 
-$driver = new NetworkDriver($dispatcher);
-$server->attach($driver, 'tcp://0.0.0.0', 8080);
-$server->attach($driver, 'udp://0.0.0.0', 12345);
+$driver = new NetworkDriver(Scheme::TCP, '0.0.0.0', 8080);
+$server->attach($driver);
+// $server->attach($driver, 'udp://0.0.0.0', 12345);
 // $server->attach($secureListener, '0.0.0.0', 8443, $secureCtx);
 // $server->attach($udpDriver, '0.0.0.0', 12345);
 
 $server->start();
-scheduler()->start();
